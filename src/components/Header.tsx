f9898f84283d7d84b738c8806c1cdfd580ff7e21
daniel <danielhenriquedhjpgithub@gmail.com>
2025-05-14 15:24:33 -0300

    tentar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,17 @@
  export function Header(){
    return(
    <header>
-    {/* {}
-    <script async  src="https:"
-    <ins className="//example.adsterra.com/display.js"></script>
+     {<><script type="text/javascript">
+               atOptions = {'key'} : 'c197949743cfcf8bb44007c92a75826b',
+               'format' : 'iframe',
+               'height' : 250,
+               'width' : 300,
+               'params' :
+               };
+           </script><script type="text/javascript" src="//www.highperformanceformat.com/c197949743cfcf8bb44007c92a75826b/invoke.js"></script></>}
+    <script async  src="https:"<ins className="//example.adsterra.com/display.js"></script>
     <ins className="adsbyadsterra" data-ad-format="link"></ins>
-    <script>(adsbyadsterra = window.adsbyadsterra || []).push({});</script> */}
+    <script>(adsbyadsterra = window.adsbyadsterra || []).push({});</script> 
    </header>
    )
 }
